740d8387eb432264f84577264643cd39c513b0bf
guatty <44141063+guatty@users.noreply.github.com>
2022-09-05 19:39:34 +0200

    removed https

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ app.use(router).use(store).mount('#app');
 
 app.config.globalProperties.$moment = moment;
 app.config.globalProperties.$tsHm = (ts) => moment.utc(ts * 1000).format('HH[h]mm');
-app.config.globalProperties.$API_URL = process.env.VUE_APP_APIURL === undefined ? "https://www.apecs.ml" : process.env.VUE_APP_APIURL;
+app.config.globalProperties.$API_URL = process.env.VUE_APP_APIURL;
 
 app.config.globalProperties.$filters = {
 	date_to_fr(value) {
